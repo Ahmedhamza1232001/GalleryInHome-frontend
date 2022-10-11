38045cd41f95db757a3700681040ae7aa7e88e69
abdelslam ahmed <abuelwafaabdelsalam@gmail.com>
2022-10-11 16:51:47 +0200

    using useForm to applay the validation

--- online-store/src/pages/login.js
+++ online-store/src/pages/login.js
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import { useForm } from "react-hook-form";
 import {Link} from "react-router-dom"
 import "./login.css"
 
 function LogIn() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const { register, handleSubmit,  formState: { errors }   } = useForm();
     const [passShow, setPassShow] = useState(false)
-    function handleSubmit(e){
-        e.preventDefault()
-
+    const submition= (data)=>{
+        // e.preventDefault()
+        console.log(typeof(data.email));    
     }
 
-        
+// function to show and hide password
 const togglePass=(e)=>{
-    e.stopPropagation()
-    let eye = document.getElementById("showPass")
+    // select the input and the icon
     let passInput = document.getElementById("password")
+    let eye = document.getElementById("showPass")
+    // toggle the state value 
     setPassShow(!passShow)
+    // set a condition if state is true or otherwise  
     if(passShow){
+        // if state is true then remove the icon eye-slash class and add the eye class    
         eye.classList.remove("fa-eye-slash")
         eye.classList.add("fa-eye")
+        // and switch the input from password to text 
         passInput.setAttribute("type","text")
     }
     else{
+        // if state is false then remove the icon eye class and add the eye-slash class     
         eye.classList.remove("fa-eye")
         eye.classList.add("fa-eye-slash")
+        // and switch the input from text to password 
         passInput.setAttribute("type","password")
+
     }
 }   
 
@@ -39,19 +46,25 @@ const togglePass=(e)=>{
                         <h3 className="pt-3 font-weight-bold">Login</h3>
                     </div>
                     <div className="panel-body p-3">
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit(submition)}>
                             <div className="form-group py-2">
+                                {<p className='error'>{errors.email?.message}</p>}
                                 <div className="input-field">
                                     <span className="far fa-user p-2"></span>
-                                    <input type="email" placeholder="Enter your Email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+                                    <input type="email" name='email' placeholder="Enter your Email"
+                                    {...register("email",{required:"this is requied*",pattern:{value:/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,6}$/,message:"please enter  valid email"}})}/>
                                  </div>
+
                             </div>
                             <div className="form-group py-1 pb-2">
+                                {<p className='error'>{errors.password?.message}</p>}
+
                                 <div className="input-field">
-                                    <span className="fas fa-lock px-2"></span>
-                                    <input type="password" id='password' placeholder="Enter your Password" 
-                                    value={password} onChange={(e)=>setPassword(e.target.value)} required/> <div className="btn bg-white text-muted">
-                                        <span  onClick ={togglePass} id='showPass' className="far fa-eye-slash"></span>
+                                    <span className="fas fa-lock p-2"></span>
+                                    <input type="password" name='password' id='password' placeholder="Enter your Password" 
+                                    {...register("password",{required:"this is requied*",minLength:{value:6,message:"password must be more than 5"}})}/> 
+                                    <div>
+                                        <span onClick ={togglePass} id='showPass' className="far fa-eye-slash bg-white text-muted"></span>
                                     </div>
                                 </div>
                             </div>
