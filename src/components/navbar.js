58d4371fd84f558ce9814a40d73b8615914c0d0f
abdelslam ahmed <abuelwafaabdelsalam@gmail.com>
2022-10-21 21:08:20 +0200

    add style to the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,28 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa'
 import "./navbar.css"
+import logo from '../images/logo.webp'
 import {Link} from "react-router-dom"
 import { useGlobalContext } from '../context'
 function Navbar() {
   const {openSearch}= useGlobalContext()
   return (
-    <nav className='header-area'>
-      <Link to="/">Home</Link>
-      <Link to="/login">login</Link>
-      <Link to="/signup">signup</Link>
-      <div>
-        <button className='btn-submit' onClick={openSearch}>open search</button>
+    <nav>
+      <div className="nav-header .container">
+        <Link to="/" className='logo'><img src={logo} alt="logo"/></Link>
+        <div className="links-center">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">login</Link></li>
+            <li><Link to="/signup">signup</Link></li>
+          </ul>
+        </div>
+        <div>
+          <span className='nav-toggle'><FaBars/></span>
+          <button className='btn btn-dark' onClick={openSearch}>open search</button>
+        </div>
+     
       </div>
-
     </nav>
     )
 }
