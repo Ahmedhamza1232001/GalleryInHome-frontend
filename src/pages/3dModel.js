6f941486eafbb13db4502a2910490877c4725f7b
rababhamdyy <rababhamdy38@gmail.com>
2023-02-23 21:17:36 +0200

    Edit some properties

--- src/pages/3dModel.js
+++ src/pages/3dModel.js
@@ -3,16 +3,20 @@ import "./3dModel.css"
 
 import { OBJModel, DirectionLight } from "react-3d-viewer";
 
+
 export default function App() {
   return (
     <div className="App">
       <div style={{ margin: "auto" }}>
         <OBJModel
-          width="800"
+          width="1600"
           height="800"
-          position={{ x: 0, y: 0, z: 0 }}
+          position={{ x: 0, y: -40, z: 0 }}
+          rotation={{ x:.2 , y:0 , z:0 }}
+          enableKeys={true}
+          enableZoom={false}
           src="../images/LEATHERSOFA-2019-obj.obj"
-          // "https://dwqdaiwenqi.github.io/react-3d-viewer/site/src/lib/model/freedom.obj"
+
           onLoad={() => {
             console.log("Loading");
           }}
@@ -32,4 +36,5 @@ export default function App() {
       </div>
     </div>
   );
+
 }
