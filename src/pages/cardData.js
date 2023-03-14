05533bc4c9fd141bb687369e2f264fcc5c033f43
abdelslam ahmed <abuelwafaabdelsalam@gmail.com>
2023-03-14 17:22:34 +0200

    adding favorite page

--- src/pages/cardData.js
+++ src/pages/cardData.js
@@ -1,33 +1,38 @@
 const CardData = [
     {
         id:1,
-        price:'180$',
-        image:'./images/5.jpeg'
+        price:'180',
+        image:'images/img1.jpg',
+        desc:"Queen Tall Chest Bedroom",
+        rating:3
     },
     {
         id:2,
-        price:'180$',
-        image:'./images/img1.jpeg'
+        price:'12',
+        image:'images/img2.jpg',
+        desc:"modern chair",
+        rating:5
     },
     {
         id:3,
-        price:'180$',
-        image:'../images/5.jpeg'
+        price:'90',
+        image:'images/img3.jpg',
+        desc: "Home desk",
+        rating:4
     },
     {
         id:4,
-        price:'180$',
-        image:'../images/4.jpeg'
+        price:'10',
+        image:'images/img4.jpg',
+        desc: "Fabric Corner Suite with Sofa Bed",
+        rating:3
     },
     {
         id:5,
-        price:'180$',
-        image:'../images/6.jpg'
-    },
-    {
-        id:6,
-        price:'180$',
-        image:'../images/7.jpg'
+        price:'200',
+        image:'images/img5.jpg',
+        desc: "Fabric Click Clack Sofa Bed",
+        rating:5
     }
     
 ]
